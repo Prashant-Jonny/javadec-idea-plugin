6a933bdd592e2769c58f5108d71321de8c82987f
Andrey Ponomarev <andrey.ponomarev@gmail.com>
2013-09-19 17:46:07 +0200

    Initial commit

---
+++ src/idea/plugin/javadec/JavadecClassFileDecompiler.java
@@ -0,0 +1,48 @@
+package idea.plugin.javadec;
+
+import com.intellij.openapi.extensions.Extensions;
+import com.intellij.openapi.fileTypes.BinaryFileDecompiler;
+import com.intellij.openapi.fileTypes.ContentBasedFileSubstitutor;
+import com.intellij.openapi.fileTypes.StdFileTypes;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.vfs.VfsUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.impl.compiled.ClsFileImpl;
+import com.intellij.vcsUtil.VcsFileUtil;
+import javadec.Decompiler;
+import org.jetbrains.annotations.NotNull;
+
+public class JavadecClassFileDecompiler implements BinaryFileDecompiler {
+
+
+    private final Decompiler decompiler = new Decompiler();
+
+    @NotNull
+    @Override
+    public CharSequence decompile(VirtualFile file) {
+        assert file.getFileType() == StdFileTypes.CLASS;
+
+        final Project[] projects = ProjectManager.getInstance().getOpenProjects();
+        if (projects.length == 0) return "";
+        final Project project = projects[0];
+
+        final ContentBasedFileSubstitutor[] processors = Extensions.getExtensions(ContentBasedFileSubstitutor.EP_NAME);
+        for (ContentBasedFileSubstitutor processor : processors) {
+            if (processor.isApplicable(project, file)) {
+                return processor.obtainFileText(project, file);
+            }
+        }
+
+        try {
+            byte[] bytes = FileUtil.loadBytes(file.getInputStream());
+            return decompiler.decompile(bytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ClsFileImpl.decompile(PsiManager.getInstance(project), file);
+        }
+
+    }
+}
